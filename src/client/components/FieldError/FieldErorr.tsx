f3b70c196c842cc39a5bfadd84be72bf7cd308fa
mauro-arias <47589523+mauro-arias@users.noreply.github.com>
2024-12-17 00:21:41 -0300

    fixed tsc

--- src/client/components/FieldError/FieldErorr.tsx
+++ src/client/components/FieldError/FieldErorr.tsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { FieldErrors, FieldError as FormError } from "react-hook-form";
 
-const FieldError = ({ errors, fieldName }: { errors: any; fieldName: string }) => {
-  if (!errors[fieldName]) return null;
-  return <span className="text-red-600 px-1 text-sm">{errors[fieldName].message}</span>;
+const FieldError = ({
+  errors,
+  fieldName,
+}: {
+  errors: FieldErrors;
+  fieldName: string;
+}) => {
+  const error = errors[fieldName] as FormError | undefined;
+  const message = error?.message;
+
+  if (!message) return null;
+
+  return <span className="text-red-600 px-1 text-sm">{message as string}</span>;
 };
 
 export default FieldError;
